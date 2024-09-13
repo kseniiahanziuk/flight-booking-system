0f01a57f1343d87243f458e70cacfb627e3cd9c2
Kseniia Hanziuk <khanziuk@kse.org.ua>
2024-09-13 13:19:44 +0300

    Created a class for commands, a class for Passenger and a class for handling user input.

--- main.cpp
+++ main.cpp
@@ -113,3 +113,109 @@ public:
         cout << "Passenger name: " << passengerName << "\n";
     }
 };
+
+class Passenger {
+    string passengerName;
+    vector<Ticket> tickets;
+public:
+    Passenger(const string passengerName) : passengerName(passengerName) {}
+
+    string getPassengerName() {
+        return passengerName;
+    }
+
+    void addTicket(const Ticket &ticket) {
+        tickets.push_back(ticket);
+    }
+
+    vector<Ticket> getTickets() const {
+        return this->tickets;
+    }
+};
+
+class Commands {
+public:
+    void checkFreeSeats() {
+
+    }
+
+    void bookTicket() {
+
+    }
+
+    void returnTicket() {
+
+    }
+
+    void viewByID() {
+
+    }
+
+    void viewByName() {
+
+    }
+
+    void viewByFlight() {
+
+    }
+
+    void help() {
+        cout << "\nHere's the list of commands: \n\n";
+        cout << "~ check - check available places for the flight.\n";
+        cout << "~ book - book a ticket for a flight.\n";
+        cout << "~ return - return ticket with refund.\n";
+        cout << "~ view id - view the booking info by ticket ID.\n";
+        cout << "~ view name - view all tickets booked under a passenger name.\n";
+        cout << "~ view flight - view all booked tickets for a flight.\n";
+        cout << "~ help - view command prompt.\n";
+        cout << "~ exit - exit the program.\n";
+    }
+};
+
+class UserInput {
+private:
+    Commands commands;
+public:
+    void userInterface() {
+        string command;
+        cout << "Welcome to the Airplane Ticket Booking System.\n";
+        do {
+            cout << "\nEnter the command you want to perform: ";
+            cin >> command;
+
+            if (command == "check") {
+                commands.checkFreeSeats();
+            }
+            else if (command == "book") {
+                commands.bookTicket();
+            }
+            else if (command == "return") {
+                commands.returnTicket();
+            }
+            else if (command == "view id") {
+                commands.viewByID();
+            }
+            else if (command == "view name") {
+                commands.viewByName();
+            }
+            else if (command == "view flight") {
+                commands.viewByFlight();
+            }
+            else if (command == "help") {
+                commands.help();
+            }
+            else if (command == "exit") {
+                cout << "Thanks for using the program. Bye!\n";
+            }
+            else {
+                cout << "Invalid input. Try using 'help' to see commands.";
+            }
+        } while (command != "exit");
+    }
+};
+
+int main() {
+    UserInput ui;
+    ui.userInterface();
+    return 0;
+}
